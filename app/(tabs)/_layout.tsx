3a71ea5d441febe43bc87f9841cb3921283ab822
ciemnv <cios.kamil0@gmail.com>
2026-06-05 20:54:11 +0200

    konfiguracja dolnej nawigacji

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,57 +1,48 @@
+import { Tabs } from 'expo-router';
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
-
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import { Platform } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
+        headerShown: true,
+        tabBarActiveTintColor: '#0284c7', // Kolor dla aktywnych zakładek
+        tabBarInactiveTintColor: '#64748b', // Kolor dla nieaktywnych zakładek
+        tabBarStyle: Platform.select({
+          ios: { position: 'absolute' },
+          default: {},
+        }),
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
+          title: 'Zadania',
+          tabBarIcon: ({ focused }) => (
+            // Ikona do listy zadań z pakietu Ionicons https://ionic.io/ionicons
+            <Ionicons name="list-outline" size={28} color={focused ? "#0284c7" : '#64748b'} />
           ),
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="calendar"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Kalendarz',
+          tabBarIcon: ({ focused }) => (
+            // Ikona do kalendarza zadań z pakietu Ionicons
+            <Ionicons name="calendar-number-outline" size={28} color={focused ? "#0284c7" : '#64748b'} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Ustawienia',
+          tabBarIcon: ({ focused }) => (
+            // Ikona do ustawień
+            <Ionicons name="settings-outline" size={28} color={focused ? "#0284c7" : '#64748b'} />
+          ),
         }}
       />
     </Tabs>
