de6332d00fcd50abec6eaa6fb82ad7224fa59f76
ciemnv <cios.kamil0@gmail.com>
2026-06-08 23:44:25 +0200

    proba konfiguracji jest - pozytywna

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   preset: 'jest-expo',
-  setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@expo/vector-icons/.*|expo-router/.*|nativewind/.*|react-native-css-interop/.*)',
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
+  },
+  fakeTimers: {
+    enableGlobally: true,
   },
 };
